e2c38c738182de693bb8cd3bf065d2fe7494bcb4
sitongfeng <sitongfeng@gmail.com>
2022-02-05 23:44:25 -0800

    feat: simplified gru_cell model code

--- Benitoite/network_model/gru_cell.py
+++ Benitoite/network_model/gru_cell.py
@@ -1,11 +1,12 @@
+import tensorflow.keras
 import tensorflow as tf
 import tensorflow.keras.layers as KL
 from tensorflow.keras.models import Model
-import tensorflow.keras
-import numpy as np
+from tensorflow import sigmoid, tanh, add
 
 emb_dim = 26
 hidden_dim = 26
+
 
 class GRU:
     def __init__(self, emb_dim=26, hidden_dim=26):
@@ -28,22 +29,22 @@ class GRU:
         self.bh = tf.random.uniform([self.hidden_dim])
 
     def reset_gate(self, x, h):
-        x_r = tf.keras.layers.Dense(self.hidden_dim, activation=None, use_bias=True,kernel_initializer='glorot_uniform',bias_initializer='glorot_uniform')(x)
-        h_r = tf.keras.layers.Dense(self.hidden_dim, activation=None, use_bias=True,kernel_initializer='glorot_uniform',bias_initializer='glorot_uniform')(h)
-        reset = tf.sigmoid(tf.add(x_r, h_r))
+        x_r = KL.Dense(self.hidden_dim, activation=None, use_bias=True)(x)
+        h_r = KL.Dense(self.hidden_dim, activation=None, use_bias=True)(h)
+        reset = sigmoid(add(x_r, h_r))
         return reset
 
     def update_gate(self, x, h):
-        x_u = tf.keras.layers.Dense(self.hidden_dim, activation=None, use_bias=True,kernel_initializer='glorot_uniform',bias_initializer='glorot_uniform')(x)
-        h_u = tf.keras.layers.Dense(self.hidden_dim, activation=None, use_bias=True,kernel_initializer='glorot_uniform',bias_initializer='glorot_uniform')(h)
-        update = tf.sigmoid(tf.add(x_u, h_u))
+        x_u = KL.Dense(self.hidden_dim, activation=None, use_bias=True)(x)
+        h_u = KL.Dense(self.hidden_dim, activation=None, use_bias=True)(h)
+        update = sigmoid(add(x_u, h_u))
         return update
 
     def new_memory_cell(self, x, h, r):
-        x_n = tf.keras.layers.Dense(self.hidden_dim, activation=None, use_bias=True,kernel_initializer='glorot_uniform',bias_initializer='glorot_uniform')(x)
+        x_n = KL.Dense(self.hidden_dim, activation=None, use_bias=True)(x)
         h = r * h
-        h_n = tf.keras.layers.Dense(self.hidden_dim, activation=None, use_bias=True,kernel_initializer='glorot_uniform',bias_initializer='glorot_uniform')(h)
-        gate_update = tf.tanh(tf.add(x_n, h_n))
+        h_n = KL.Dense(self.hidden_dim, activation=None, use_bias=True)(h)
+        gate_update = tanh(add(x_n, h_n))
         return gate_update
 
     def forward(self, x, h):
@@ -56,32 +57,10 @@ class GRU:
         current_hidden_state = KL.Add()([mul1, mul2])
         return current_hidden_state
 
-
-def representative_dataset_gen():
-    for _ in range(8):
-        # Get sample input data as a numpy array in a method of your choosing.
-        input_data_1 = np.array(np.random.random_sample((1, emb_dim)), dtype=np.float32)
-        input_data_2 = np.array(np.random.random_sample((1, hidden_dim)), dtype=np.float32)
-        yield [input_data_1, input_data_2]
-
-gru = GRU(emb_dim, hidden_dim)
-i = tensorflow.keras.Input(shape=[emb_dim], batch_size=1)
-h = tensorflow.keras.Input(shape=[hidden_dim], batch_size=1)
-new_h_t = gru.forward(i, h)
-model = Model([i, h], new_h_t)
-model.summary()
-# x = tf.ones((1, 26))
-# x2 = tf.ones((1, 26))
-# y = model(x, x2)
-# tf.print(x)
-# tf.print(y)
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.representative_dataset = representative_dataset_gen
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-# converter.target_spec.supported_ops = [tf.lite.OpsSet.EXPERIMENTAL_TFLITE_BUILTINS_ACTIVATIONS_INT16_WEIGHTS_INT8]
-converter.experimental_new_converter = False
-converter.experimental_new_quantizer = False
-tflite_quant_model = converter.convert()
-open("./gru_int8.tflite", "wb").write(tflite_quant_model)
+    def model(self):
+        gru = GRU(emb_dim, hidden_dim)
+        i = tensorflow.keras.Input(shape=[emb_dim], batch_size=1)
+        h = tensorflow.keras.Input(shape=[hidden_dim], batch_size=1)
+        new_h_t = gru.forward(i, h)
+        model = Model([i, h], new_h_t)
+        return model
